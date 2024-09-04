c67b2f80cc93786439eb4e40d2c58f3c7dd1fe6f
Embrace IT <embraceit@Umairs-Macbook.local>
2024-09-04 15:41:05 +0500

    final

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,4 +1,4 @@
-import TagLine from "./Tagline";
+import TagLine from "/Users/embraceit/Desktop/BrainWave/src/components/Tagline.jsx";
 
 const Heading = ({ className, title, text, tag }) => {
   return (
